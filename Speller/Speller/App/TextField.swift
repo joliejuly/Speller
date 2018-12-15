1a21852739b12203c868bf4119c36d66fe78eecc
Julia Nikitina <julianikitina.ios@gmail.com>
2018-12-15 23:30:07 +0300

    Clean up TextField class

--- Speller/Speller/App/TextField.swift
+++ Speller/Speller/App/TextField.swift
@@ -14,16 +14,19 @@ class TextField: UITextField {
     let inset: CGFloat = 20
     
     // placeholder position
-    override func textRect(forBounds bounds: CGRect) -> CGRect {
+    override func textRect(
+        forBounds bounds: CGRect) -> CGRect {
         return bounds.insetBy(dx: inset, dy: 0)
     }
     
     // text position
-    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+    override func editingRect(
+        forBounds bounds: CGRect) -> CGRect {
         return bounds.insetBy(dx: inset, dy:0)
     }
     
-    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+    override func placeholderRect(
+        forBounds bounds: CGRect) -> CGRect {
         return bounds.insetBy(dx: inset, dy:0)
     }
 }
